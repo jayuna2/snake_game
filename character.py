8a8be2f47d8b856e84bafe469110ecec95499fec
Jayuna Khatiwada <khatiwadajayuna400@gmail.com>
2025-11-29 23:35:04 -0600

    Saving edits on character and main module

--- character.py
+++ character.py
@@ -1,16 +1,16 @@
 import pygame
 
-head_size=[400,250,14,18] #x, y, width, height
-snake_colour =(135, 206, 235) #RGB
+head_size=[400,250,16,16] #x, y, width, height
+
 x_head,y_head, head_width, head_height = head_size
 
 body_width = head_width
 body_height= head_height -4
-x_body= x_head- body_width
-y_body = y_head +2
+x_body= x_head
+y_body = y_head
 
-current_direction= 'RIGHT' #initial direction of snake
-snake_speed = 10
+current_direction ='RIGHT' #initial position of the snake 
+change_direction = 15
 
 snake_parts =[
     [x_head, y_head, head_width, head_height],
@@ -19,8 +19,76 @@ snake_parts =[
 
 def snake_drawing(window):
     global snake_parts
+    body_colour =(105, 163, 225) #RGB
+    head_colour= (81, 123, 204)
 
-    for part in snake_parts:
-        pygame.draw.rect(window, snake_colour, part, 0)
+    if snake_parts:
+        head_part = snake_parts[0]
+        pygame.draw.rect(window, head_colour, head_part, 0)
+
+        for i in range (1, len(snake_parts)):
+            body_part = snake_parts[i]
+            pygame.draw.rect(window, body_colour, body_part, 0)
+
+
+def snake_position():
+    global snake_parts, current_direction
+
+    new_head = snake_parts[0].copy()
+
+    if current_direction =='RIGHT':
+        new_head[0] += change_direction
+    elif current_direction =='LEFT':
+        new_head[0] -= change_direction
+    elif current_direction =='UP':
+        new_head[1] -= change_direction
+    elif current_direction == 'DOWN':
+        new_head[1] += change_direction
+
+    snake_parts.insert(0, new_head)
+    snake_parts.pop()
+
+def user_input(events):
+    global current_direction
+
+    for event in events:
+        if event.type==pygame.KEYDOWN:
+            new_direction =current_direction
+
+            if event.key == pygame.K_RIGHT:
+                if current_direction != 'LEFT':
+                    new_direction = 'RIGHT'
+            elif event.key == pygame.K_LEFT:
+                if current_direction != 'RIGHT':
+                    new_direction = 'LEFT'
+            elif event.key == pygame.K_UP:
+                if current_direction!='DOWN':
+                    new_direction = 'UP'
+            elif event.key == pygame.K_DOWN:
+                if current_direction != 'UP':
+                    new_direction = 'DOWN'
+            
+            current_direction = new_direction
+
+
+def check (screen_size):
+    screen_width= screen_size[0]
+    screen_height = screen_size[1]
+    global snake_parts
+
+    head_x_position = snake_parts[0][0]
+    head_y_position = snake_parts [0][1]
+
+    if not snake_parts:
+        return False
+    
+    if head_x_position<0 or head_x_position > screen_width:
+        return True
+    
+    if head_y_position<0 or head_y_position > screen_height:
+        return True
+    
+    return False
+
 
 
--- main.py
+++ main.py
@@ -11,17 +11,30 @@ def main_screen():
 
 def game_loop():
     start = True
+    game_over = False
     window = main_screen()
+    screen_size = window.get_size()
+
+    clock =pygame.time.Clock()
+    SPEED = 8
     while start:
         events= pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 start = False
-        
+
+        if not game_over:    
+            character.user_input(events)
+            character.snake_position()
+
+        if character.check(screen_size):
+            game_over = True
+
+        window.fill((0,0,0))
         character.snake_drawing(window)
         pygame.display.update()
-        window.fill((0,0,0))
-        
+        clock.tick(SPEED)
 
+    pygame.quit()
 
 game_loop()
