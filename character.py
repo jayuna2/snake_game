6b8d9a0ef1ad500fd6f1c8e3ac9b8b681fe41393
ManojKumar@28 <139015959+Manojkumarcherukuneedi@users.noreply.github.com>
2025-12-03 11:39:51 -0600

    Update character.py

--- character.py
+++ character.py
@@ -1,46 +1,46 @@
 import pygame
 
-head_size=[400,250,16,16] #x, y, width, height
 
-x_head,y_head, head_width, head_height = head_size
+head_size = [400, 250, 20, 20]
+x_head, y_head, head_width, head_height = head_size
 
-body_width = head_width
-body_height= head_height -4
-x_body= x_head
+
+body_width = 20
+body_height = 20
+x_body = x_head - 20
 y_body = y_head
 
-current_direction ='RIGHT' #initial position of the snake 
-change_direction = 15
+current_direction = 'RIGHT'
+change_direction = 20  
 
-snake_parts =[
+
+snake_parts = [
     [x_head, y_head, head_width, head_height],
     [x_body, y_body, body_width, body_height]
 ]
 
 def snake_drawing(window):
-    global snake_parts
-    body_colour =(105, 163, 225) #RGB
-    head_colour= (81, 123, 204)
+    head_color = (81, 123, 204)
+    body_color = (105, 163, 225)
 
-    if snake_parts:
-        head_part = snake_parts[0]
-        pygame.draw.rect(window, head_colour, head_part, 0)
+    
+    pygame.draw.rect(window, head_color, snake_parts[0], 0)
 
-        for i in range (1, len(snake_parts)):
-            body_part = snake_parts[i]
-            pygame.draw.rect(window, body_colour, body_part, 0)
-
+    
+    for part in snake_parts[1:]:
+        pygame.draw.rect(window, body_color, part, 0)
 
 def snake_position():
+    """Move the snake by inserting a new head and removing the last part."""
     global snake_parts, current_direction
 
     new_head = snake_parts[0].copy()
 
-    if current_direction =='RIGHT':
+    if current_direction == 'RIGHT':
         new_head[0] += change_direction
-    elif current_direction =='LEFT':
+    elif current_direction == 'LEFT':
         new_head[0] -= change_direction
-    elif current_direction =='UP':
+    elif current_direction == 'UP':
         new_head[1] -= change_direction
     elif current_direction == 'DOWN':
         new_head[1] += change_direction
@@ -52,43 +52,31 @@ def user_input(events):
     global current_direction
 
     for event in events:
-        if event.type==pygame.KEYDOWN:
-            new_direction =current_direction
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT and current_direction != 'LEFT':
+                current_direction = 'RIGHT'
+            elif event.key == pygame.K_LEFT and current_direction != 'RIGHT':
+                current_direction = 'LEFT'
+            elif event.key == pygame.K_UP and current_direction != 'DOWN':
+                current_direction = 'UP'
+            elif event.key == pygame.K_DOWN and current_direction != 'UP':
+                current_direction = 'DOWN'
 
-            if event.key == pygame.K_RIGHT:
-                if current_direction != 'LEFT':
-                    new_direction = 'RIGHT'
-            elif event.key == pygame.K_LEFT:
-                if current_direction != 'RIGHT':
-                    new_direction = 'LEFT'
-            elif event.key == pygame.K_UP:
-                if current_direction!='DOWN':
-                    new_direction = 'UP'
-            elif event.key == pygame.K_DOWN:
-                if current_direction != 'UP':
-                    new_direction = 'DOWN'
-            
-            current_direction = new_direction
+def check(screen_size):
+    """Return True if snake hits wall or its own body."""
+    width, height = screen_size
+    head_x = snake_parts[0][0]
+    head_y = snake_parts[0][1]
 
-
-def check (screen_size):
-    screen_width= screen_size[0]
-    screen_height = screen_size[1]
-    global snake_parts
-
-    head_x_position = snake_parts[0][0]
-    head_y_position = snake_parts [0][1]
-
-    if not snake_parts:
-        return False
     
-    if head_x_position<0 or head_x_position > screen_width:
+    if head_x < 0 or head_x >= width:
         return True
-    
-    if head_y_position<0 or head_y_position > screen_height:
+    if head_y < 0 or head_y >= height:
         return True
+
     
+    for part in snake_parts[1:]:  
+        if head_x == part[0] and head_y == part[1]:
+            return True
+
     return False
-
-
-
